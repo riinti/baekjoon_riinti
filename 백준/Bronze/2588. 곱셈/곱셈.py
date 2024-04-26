ad800c012f8b4f70328017c36600835f2e8acf8e
riinti <165676114+riinti@users.noreply.github.com>
2024-04-26 13:07:56 +0900

    [Bronze III] Title: 곱셈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,7 @@
+num1=int(input())
+num2=input()
+
+print(int(num2[2])*num1)
+print(int(num2[1])*num1)
+print(int(num2[0])*num1)
+print(int(num2[0])*num1*100+int(num2[1])*num1*10+int(num2[2])*num1*1)
