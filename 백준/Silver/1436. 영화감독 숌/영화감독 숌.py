ff6299464510b2fe0819c693981ee9b955788a77
riinti <165676114+riinti@users.noreply.github.com>
2024-05-08 18:52:46 +0900

    [Silver V] Title: 영화감독 숌, Time: 576 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -3,14 +3,14 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-count = 0
+count = 1
 num = 0
 
 while True:
   num+=1
-  if count == N:
-    print(int(num)-1)
-    break
-  else:
-    if "666" in str(num):
+  if "666" in str(num):
+    if count == N:
+      print(int(num))
+      break
+    else:
       count+=1
