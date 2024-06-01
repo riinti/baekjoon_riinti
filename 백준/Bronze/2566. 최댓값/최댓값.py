b49facde0f598699d5b46a7188402b707747cf4e
riinti <165676114+riinti@users.noreply.github.com>
2024-06-01 22:48:43 +0900

    [Bronze III] Title: 최댓값, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.py
@@ -0,0 +1,13 @@
+n = [list(map(int, input().split())) for _ in range(9)]
+
+max_num = 0
+max_row, max_col = 0, 0
+for row in range(9):
+    for col in range(9):
+        if max_num <= n[row][col]:
+            max_row = row + 1
+            max_col = col + 1
+            max_num = n[row][col]
+
+print(max_num)
+print(max_row, max_col)
