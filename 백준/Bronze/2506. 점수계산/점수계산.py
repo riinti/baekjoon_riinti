11e673f46f5a699852408f3f89d6631cba83f22f
riinti <165676114+riinti@users.noreply.github.com>
2024-06-22 12:47:43 +0900

    [Bronze III] Title: 점수계산, Time: 76 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2506. 점수계산/점수계산.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+data = list(map(int, input().split()))
+
+count = 0
+score = 0
+for i in range(n) :
+  if data[i] == 1 :
+    count += 1
+    score += count
+  else :
+    count = 0
+
+print(score)
