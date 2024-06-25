7615cd7e0cdb859d2309aca2908e549e9cf6096a
riinti <165676114+riinti@users.noreply.github.com>
2024-06-25 14:19:46 +0900

    [Bronze III] Title: 네 번째 점, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+
+X = []
+Y = []
+
+for i in range(3):
+    x, y = map(int, input().split())
+    X.append(x)
+    Y.append(y)
+
+for i in range(3):
+    if X.count(X[i]) == 1:
+        x = X[i]
+    if Y.count(Y[i]) == 1:
+        y = Y[i]
+
+print(x, y)
