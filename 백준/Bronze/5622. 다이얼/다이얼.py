11c55eda94b35e4bdaba687a96a3fcd637bb6955
riinti <165676114+riinti@users.noreply.github.com>
2024-07-03 13:56:38 +0900

    [Bronze II] Title: 다이얼, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+
+dial = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
+word = input()
+time = 0
+for j in range(len(word)):
+    for i in dial:
+        if word[j] in i:
+            time += dial.index(i)+3
+print(time)
