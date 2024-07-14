d2db2e04ddcf979398ac54383f5681ab1dd9c407
riinti <165676114+riinti@users.noreply.github.com>
2024-07-14 21:08:30 +0900

    [Bronze II] Title: 운동, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1173. 운동/운동.py
@@ -0,0 +1,19 @@
+N, m, M, T, R = map(int, input().split())
+
+time = total = 0
+heart = m
+
+if m + T > M:
+    print(-1)
+else:
+    while time < N:
+        if heart + T <= M:
+            heart += T
+            time += 1
+            total += 1
+        else:
+            heart -= R
+            if heart < m:
+                heart = m
+            total += 1
+    print(total)
