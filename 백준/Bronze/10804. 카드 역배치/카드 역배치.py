e80b48e5be68d625fdb7db28ed9e3b18fbe614aa
minsoo <165676114+riinti@users.noreply.github.com>
2024-09-19 14:54:10 +0900

    [Bronze II] Title: 카드 역배치, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10804. 카드 역배치/카드 역배치.py
@@ -0,0 +1,10 @@
+import sys
+
+input = sys.stdin.readline
+
+cards = [i for i in range(1,21)]
+for _ in range(10):
+    a,b = map(int,input().split())
+    a-=1
+    cards[a:b] = cards[a:b][::-1]
+print(*cards)
