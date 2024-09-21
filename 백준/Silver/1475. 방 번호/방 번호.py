216eab868cd938dded0850dec2a928da9e005854
minsoo <165676114+riinti@users.noreply.github.com>
2024-09-21 15:10:10 +0900

    [Silver V] Title: 방 번호, Time: 32 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/1475. 방 번호/방 번호.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+room_num = input().rstrip()
+arr = [0] * 10
+for i in range(len(room_num)):
+    num = int(room_num[i])
+    if num == 6 or num == 9:
+        if arr[6] <= arr[9]:
+            arr[6] += 1
+        else:
+            arr[9] += 1
+    else:
+        arr[num] += 1
+print(max(arr))
