97cae8b06ce86f7c1efcf4d30fe587494c97271b
minsoo <165676114+riinti@users.noreply.github.com>
2024-09-24 17:17:52 +0900

    [Silver IV] Title: 큐 2, Time: 1580 ms, Memory: 175072 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,35 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+N = int(input())
+nlist = deque()
+
+for _ in range(N):
+  order=list(input().split())
+  if order[0]=="push":
+    nlist.append(order[1]) 
+  elif order[0]=="pop":
+    if len(nlist)==0:
+      print(-1)
+    else:
+      ind=nlist.popleft()
+      print(ind)
+  elif order[0]=="size":
+    print(len(nlist))
+  elif order[0]=="empty":
+    if len(nlist)==0:
+      print(1)
+    else:
+      print(0)
+  elif order[0]=="front":
+    if len(nlist)==0:
+      print(-1)
+    else:
+      print(nlist[0])
+  elif order[0]=="back":
+    if len(nlist)==0:
+      print(-1)
+    else:
+      print(nlist[-1])
